1b23e6fb5f5e880018cdf15e199eb2e9a9856d38
DESKTOP-I1OU07J\Khanh <duykhanh.doc@gmail.com>
2025-06-09 06:56:00 +0700

    Revert "Prepare for deployment"
    This reverts commit 33c21833c4e82ed0710570045f6ba685a113d8f8.

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
@@ -6,7 +5,14 @@ import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 
+<<<<<<< HEAD
 dotenv.config();
+=======
+// Load env vars
+dotenv.config();
+
+// Connect to database
+>>>>>>> parent of 33c2183 (Prepare for deployment)
 connectDB();
 
 const app = express();
@@ -20,6 +26,7 @@ app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID);
 });
 
+<<<<<<< HEAD
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === 'production') {
@@ -36,3 +43,20 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+=======
+// Mount the routers
+app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/orders', orderRoutes); 
+
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in development mode on port ${PORT}`)
+);
+>>>>>>> parent of 33c2183 (Prepare for deployment)
