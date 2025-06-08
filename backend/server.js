84a03ad34c47da56dbf41744e40bd84ff1e601d9
DESKTOP-I1OU07J\Khanh <duykhanh.doc@gmail.com>
2025-06-09 05:07:53 +0700

    DEBUG: Disabling all API routes to find crash source

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import path from 'path';
+/* import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js'; // Note the path
@@ -56,4 +56,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
   console.log(`Server running in development mode on port ${PORT}`)
-);
+); */
